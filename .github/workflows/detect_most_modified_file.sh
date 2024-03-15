930580f08a539f2ec60d205f0fe954c7bca1d8c2
DraftMan <nicovanaarsen@gmail.com>
2024-03-15 22:24:11 +0100

    fix: 🐛 Fix issue

---
+++ .github/workflows/detect_most_modified_file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+diff_output=$(git diff --name-only main "$current_branch")
+if [ -z "$diff_output" ]; then
+  echo "Aucun fichier modifié trouvé dans le diff entre 'main' et la branche actuelle."
+  exit 0
+fi
+
+most_modified_file=""
+max_modifications=0
+for file in $diff_output; do
+  modifications=$(git diff --numstat main "$current_branch" -- $file | awk '{print $1+$2}')
+  if [ $modifications -gt $max_modifications ]; then
+    max_modifications=$modifications
+    most_modified_file=$file
+  fi
+done
+
+echo "$most_modified_file" | sed -E 's/[0-9]+\.//g' | sed 's/\.md$//'
